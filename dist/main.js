2ede11d53767903b2416e1422b999ba07c223a61
Mariano Daniel Gobea Alcoba <gobeamariano@gmail.com>
2023-01-16 17:35:14 -0300

    second commit 16.1.2023

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("hello"),console.log(sunm(2,2))})();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import sum from './utils/sum.js'
+
+const hello = 'hello';
+
+console.log(hello);
+console.log(sunm(2,2));
